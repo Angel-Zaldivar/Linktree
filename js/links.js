737b42615e436d57c506edb660c4c4214dbaf09d
Angel Zaldivar <ZaldivarAngel.2004@gmail.com.>
2024-10-22 21:12:26 -0700

    removed tiny images

--- js/links.js
+++ js/links.js
@@ -3,28 +3,20 @@ export const links = [
         name: "Website Portfolio",
         link:
 "https://azaldivar-portfolio.netlify.app/",
-        image:
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
     {
         name: "Resume",
         link:
 "https://twitter.com/geeksforgeeks",
-        image:
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
     {
         name: "Linkedin",
         link:
 "https://www.linkedin.com/in/z-angel/",
-        image:
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
     {
         name: "E-mail",
         link:
-"mailto:zaldivarangel.2004@gmail.com",
-        image:
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
+            "mailto:zaldivarangel.2004@gmail.com"
+    }
 ];
